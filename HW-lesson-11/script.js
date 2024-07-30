3a22616ce67b81e38a9ba09ce5ee4444ccadedaa
valerimak <valerimak2000@gmail.com>
2024-07-30 16:07:10 +0300

    Add files via upload

---
+++ HW-lesson-11/script.js
@@ -0,0 +1,34 @@
+//сумма
+function sum(a, b) {
+    return a + b;
+}
+let result1 = sum(6, 7);
+console.log(result1);
+
+//разница
+function diff(a, b) {
+    return a - b;
+}
+let result2 = diff(7, 2);
+console.log(result2);
+
+//произведение
+function multi(a, b) {
+    return a * b;
+}
+let result3 = multi(5, 6);
+console.log(result3);
+
+//деление
+function div(a, b) {
+    return a / b;
+}
+let result4 = div(80, 8);
+console.log(result4);
+
+//степень
+function pow(a, b) {
+    return a ** b;
+}
+let result5 = pow(5, 2);
+console.log(result5);
