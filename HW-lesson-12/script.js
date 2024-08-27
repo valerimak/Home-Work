54a1d7c1d6d32343587b4cce34e0a72bd8ab9695
valerimak <valerimak2000@gmail.com>
2024-08-27 16:14:25 +0300

    Add files via upload

---
+++ HW-lesson-12/script.js
@@ -0,0 +1,18 @@
+let a = 4;
+let b = 1;
+
+(a + b < 4) ? console.log('Мало') : console.log('Много')
+
+
+let login = prompt('Введите логин');
+
+(login == 'Сотрудник') ? console.log('Привет')
+    : (login == 'Директор') 
+        ? console.log('Здравствуйте') 
+        : (login == '') 
+                ? console.log('Нет логина') 
+                : console.log('')
+    
+
+
+
